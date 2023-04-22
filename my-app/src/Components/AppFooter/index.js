3467249b9a6de7a2115c042a7048ea246b492053
dewsiro000 <norawit1267@gmail.com>
2023-04-23 00:05:30 +0700

    #2  จัดวาง component
    # 1.add Navbar
       2.add SideMenu
       3.add Footer
       4.add headder icon

--- my-app/src/Components/AppFooter/index.js
+++ my-app/src/Components/AppFooter/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Typography } from "antd";
+
+
 
 const AppFooter = () => {
     return (
-        <div className='AppFooter'>
-            Footer
+        <div className="AppFooter">
+            <Typography.Link href="tel:+123456789">+123456789</Typography.Link>
+            <Typography.Link href="https://www.google.com" target={"_blank"}>
+                Privacy Policy
+            </Typography.Link>
+            <Typography.Link href="https://www.google.com" target={"_blank"}>
+                Terms of Use
+            </Typography.Link>
         </div>
     )
 }
--- my-app/src/Components/AppHeader/index.js
+++ my-app/src/Components/AppHeader/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BellFilled, MailOutlined } from "@ant-design/icons";
+import { Badge, Drawer, Image, List, Space, Typography } from "antd";
 
 const AppHeader = () => {
     return (
         <div className='AppHeader'>
-            AppHeader
+            <Image
+                width={40}
+                src="https://yt3.ggpht.com/ytc/AMLnZu83ghQ28n1SqADR-RbI2BGYTrqqThAtJbfv9jcq=s176-c-k-c0x00ffffff-no-rj"
+            ></Image>
+            <Typography.Title>Aamir's Dashboard</Typography.Title>
+            <Space>
+                <Badge count={10} dot>
+                    <MailOutlined style={{ fontSize: 24 }} />
+                </Badge>
+                <Badge count={20}>
+                    <BellFilled style={{ fontSize: 24 }} />
+                </Badge>
+            </Space>
         </div>
     )
 }
